72c71180e85ba6a405e29753bb778bcb121f0d82
Petr Hovorka <pehovorka@gmail.com>
2022-04-03 13:43:56 +0100

    💬 FE: Add link to API docs

--- event-registration-frontend/src/components/footer/Footer.tsx
+++ event-registration-frontend/src/components/footer/Footer.tsx
@@ -1,4 +1,7 @@
+import { SelectOutlined } from "@ant-design/icons";
+import { Divider } from "antd";
 import { NavLink } from "react-router-dom";
+import { API_BASE_URL } from "../../config/api";
 import { route } from "../../Routes";
 
 interface Props {
@@ -13,10 +16,21 @@ function Footer({ isAdminPath }: Props) {
       ) : (
         <NavLink to={route.events}>Switch to attendee client</NavLink>
       )}
-      <br />
-      Client app created for the CSCU9YW - Web Services module assignment.
-      <br />
-      Student number 2944806, University of Stirling, spring semester 2022.
+      <Divider type="vertical" />
+      <a
+        href={`${API_BASE_URL}/swagger-ui/index.html`}
+        rel="noreferrer"
+        target="_blank"
+      >
+        API docs <SelectOutlined rotate={90} />
+      </a>
+      <Divider style={{ margin: "1rem" }} />
+      <p>
+        Client app created for the CSCU9YW - Web Services module assignment.
+      </p>
+      <p>
+        Student number 2944806, University of Stirling, spring semester 2022.
+      </p>
     </div>
   );
 }
